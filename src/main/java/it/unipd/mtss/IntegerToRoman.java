7677a90b16f4d41027cc619c2df7bdf716a572d7
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-05-15 16:57:57 +0200

    close #4 Inizializzazione progetto

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// Marco Brugin 2010012
+// Riccardo Rossi 2000554
+//////////////////////////////////////////////////////////////////// 
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+   
+    public static String convert(int number){
+        // TODO
+        return null;
+    }
+}
